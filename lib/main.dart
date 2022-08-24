190728e09ce29802c122f272246a96104a361459
VioMoreno <viomoreno3@gmail.com>
2022-08-24 09:49:37 -0300

    routes3

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fl_componentes/router/app_routes.dart';
 import 'package:fl_componentes/screens/screens.dart';
 
 void main() => runApp(const MyApp());
@@ -11,18 +12,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       //home: const CardScreen(), 
-      initialRoute: 'home' ,
-      routes: {
-       'listView1': (BuildContext context) => const Listview1Screen (),
-       'listView2': (BuildContext context) => const Listview2Screen (),
-       'Alert'    : (BuildContext context) => const AlertScreen (),
-       'Card'     : (BuildContext context) => const CardScreen (),
-       'home'     : (BuildContext context) => const HomeScreen(),
-      },
-
-      onGenerateRoute:(settings) {
-        return MaterialPageRoute(builder: (context) => const AlertScreen(),); 
-      },
+      initialRoute: AppRoutes.initialRoute,
+      routes: AppRoutes.routes,
+      onGenerateRoute: AppRoutes.onGenerateRoute
+      
     );
   }
 }
