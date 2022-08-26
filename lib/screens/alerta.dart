31e29c980fd52f6055b3018403d19059b5259ed1
VioMoreno <viomoreno3@gmail.com>
2022-08-26 11:25:18 -0300

    alert2

--- lib/screens/alerta.dart
+++ lib/screens/alerta.dart
@@ -5,6 +5,34 @@ class AlertScreen extends StatelessWidget {
    
   const AlertScreen({Key? key}) : super(key: key);
   
+  void displayDialog(BuildContext context) {
+
+    showDialog(
+    barrierDismissible: false,
+    context: context, 
+    builder: (context) {
+      return AlertDialog(
+        elevation: 5,
+        title: const Text('Titulo'),
+        shape: RoundedRectangleBorder(borderRadius:BorderRadiusDirectional.circular(10) ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: const [
+            Text('Este es el contenido de la alerta'),
+            SizedBox(height: 10,),
+            FlutterLogo(size: 50),
+          ],
+        ),
+        actions: [
+          TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text ('Cancelar'),),
+        ],
+      );
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,17 +44,13 @@ class AlertScreen extends StatelessWidget {
           //   elevation: 10,
           // ),
           child: const Text('Mostrar alerta', style: TextStyle(fontSize: 15),) ,
-          onPressed: () {
-            
-          },
+          onPressed: () => displayDialog(context),
 
          )),
       floatingActionButton: FloatingActionButton(
         backgroundColor: AppTheme.primary,
-        onPressed: (() {
-          Navigator.pop(context); 
-        }),
-        child: const Icon(Icons.close) 
+        onPressed: () => Navigator.pop(context),
+        child: const Icon(Icons.close),
       ),
     );
   }
