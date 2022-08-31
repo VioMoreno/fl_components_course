0ac693179246106b58bf43085c7f97d27247c2e5
VioMoreno <viomoreno3@gmail.com>
2022-08-31 12:45:30 -0300

    Sliders&Checks

--- lib/screens/slider.dart
+++ lib/screens/slider.dart
@@ -12,6 +12,7 @@ class SliderScreen extends StatefulWidget {
 class _SliderScreenState extends State<SliderScreen> {
 
   double _sliderValue = 100; 
+  bool _sliderEnabled = true; 
 
   @override
   Widget build(BuildContext context) {
@@ -19,28 +20,66 @@ class _SliderScreenState extends State<SliderScreen> {
       appBar: AppBar(
         title: const Text('Sliders and Checks'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Slider.adaptive(
-              min: 50,
-              max:400,
-              activeColor: AppTheme.primary,
-              value: _sliderValue, 
-              onChanged: ((value) {
-                _sliderValue = value; 
-                setState(() {});
-              })),
+      body: Column(
+        children: [
+          Slider.adaptive(
+            min: 50,
+            max:400,
+            activeColor: AppTheme.primary,
+            value: _sliderValue, 
+            onChanged: ( _sliderEnabled ? (value) {
+              _sliderValue = value; 
+              setState(() {}); 
+            }
+            :null )),
       
-               Image(
-                image: const NetworkImage ('https://i.pinimg.com/736x/74/b1/8f/74b18fb795cc4b825967294441e7db03.jpg'),
-                width: _sliderValue,
-                fit: BoxFit.contain,
-              ),
+            // Checkbox(
+            //   value: _sliderEnabled, 
+            //   onChanged: ((value) {
+            //     _sliderEnabled = value ?? true; 
+            //     setState(() { });
+                
+            //   })),
 
-              const SizedBox (height: 50,)
-          ],
-        ),
+            // Switch(
+            //   value: _sliderEnabled, 
+            //   onChanged: ((value) => setState(() {
+            //     _sliderEnabled = value;
+            //   })
+              
+            //   )),
+
+            CheckboxListTile(
+              title: const Text('Habilitar Slider'),
+              activeColor: AppTheme.primary,
+              value: _sliderEnabled, 
+              onChanged: ((value) => setState(() {
+                _sliderEnabled = value ?? true;
+              })
+              )),
+            
+            SwitchListTile(
+              title: const Text('Habilitar Slider'),
+              activeColor: AppTheme.primary,
+              value: _sliderEnabled, 
+              onChanged: (value) => setState(() {
+                _sliderEnabled = value;
+               })),
+
+               const AboutListTile(), 
+            
+
+             Expanded(
+               child: SingleChildScrollView(
+                 child: Image(
+                  image: const NetworkImage ('https://i.pinimg.com/736x/74/b1/8f/74b18fb795cc4b825967294441e7db03.jpg'),
+                  width: _sliderValue,
+                  fit: BoxFit.contain,
+                  ),
+               ),
+             ),
+
+        ],
       )
     );
   }
