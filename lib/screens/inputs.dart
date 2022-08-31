3866128b684e26b85778d008448d78fb3df70a06
VioMoreno <viomoreno3@gmail.com>
2022-08-31 11:18:54 -0300

    InputsForm3

--- lib/screens/inputs.dart
+++ lib/screens/inputs.dart
@@ -8,27 +8,60 @@ class InputsScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
+
+    final GlobalKey<FormState> myFormKey = GlobalKey<FormState>(); 
+
+    final Map <String, String> formValues = {
+      'first Name': 'Violeta',
+      'last Name' : 'Moreno',
+      'email'     : 'viomoren3@gmail.com',
+      'password'  : 'Holahola123',
+      'role'      : 'Admin',
+    };
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Inputs y Forms'),
       ),
+
       body: SingleChildScrollView(
         child: Padding (
           padding: const EdgeInsets.symmetric(horizontal: 20, vertical:10),
-          child: Column(
-            children: const [
-              CustomImputField(labelText: 'Nombre', hintText: 'Nombre del usuario', helperText: 'Solo texto', counterText: '3 caracteres minimo',),
-              SizedBox (height: 30), 
+          child: Form(
 
-              CustomImputField(labelText: 'Apellido', hintText: 'Apellido del usuario', helperText: 'Solo texto', counterText: '3 caracteres minimo',),
-              SizedBox (height: 30),
-              
-              CustomImputField(labelText: 'Email', hintText: 'Email del usuario', helperText: 'Solo texto', counterText: '3 caracteres minimo', keyboardType: TextInputType.emailAddress,),
-              SizedBox (height: 30),
+            key: myFormKey,
+            child: Column(
+              children:  [
+                const CustomImputField(labelText: 'Nombre', hintText: 'Nombre del usuario', helperText: 'Solo texto', counterText: '3 caracteres minimo',),
+                const SizedBox (height: 30), 
+          
+                const CustomImputField(labelText: 'Apellido', hintText: 'Apellido del usuario', helperText: 'Solo texto', counterText: '3 caracteres minimo',),
+                const SizedBox (height: 30),
+                
+                const CustomImputField(labelText: 'Email', hintText: 'Email del usuario', helperText: 'Solo texto', counterText: '3 caracteres minimo', keyboardType: TextInputType.emailAddress,),
+                const SizedBox (height: 30),
+          
+                const CustomImputField(labelText: 'Contraseña', hintText: 'Contraseña del usuario', obscureText: true,), 
+                const SizedBox (height: 30),
+          
+                ElevatedButton(
+                  onPressed: (() {
+                    
+                    FocusScope.of(context).requestFocus(FocusNode()); 
 
-              CustomImputField(labelText: 'Contraseña', hintText: 'Contraseña del usuario', obscureText: true,), 
-              SizedBox (height: 30),
-            ],  
+                    if (!myFormKey.currentState!.validate()){
+                      return; 
+                    }
+
+                    print(formValues);
+                    
+                  }),
+                  child: const SizedBox(
+                    width: double.infinity,
+                    child: Center (child: Text('Guardar'))), 
+                  )
+              ],  
+            ),
           ),
           ),
       )
